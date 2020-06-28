0273f428b2e678d27fbb1e87131c6a7c21dbc3f3
Oil ENRES <sudarat@enres.co>
2020-06-28 23:49:05 +0700

    Parsing Incoming Events 3

--- query/index.js
+++ query/index.js
@@ -8,16 +8,30 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
   if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
   }
 
   if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
   }
+
+  console.log(posts);
+  // console.log(JSON.stringify(posts, null, 2));
+
+  res.send({});
 });
 
 app.listen(4002, () => {
