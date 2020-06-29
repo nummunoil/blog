07632c48d00c5ec437ab62da92c6ab49ca74a1d1
Oil ENRES <sudarat@enres.co>
2020-06-29 14:20:58 +0700

    Adding Comment Moderation

--- comments/index.js
+++ comments/index.js
@@ -20,7 +20,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -30,6 +30,7 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
 
@@ -37,7 +38,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 });
 
 app.post("/events", (req, res) => {
-  console.log("Event Received: ", req.body.type);
+  console.log("Event Received:", req.body.type);
 
   res.send({});
 });
